bd1073f2928f63d88299fed57ea669f7458d456e
Ritesh Mahato <87099919+Ritesh1625@users.noreply.github.com>
2022-05-17 07:13:29 +0530

    Create Chocolate Distribution Problem.cpp

---
+++ Day-2/Arrays/Chocolate Distribution Problem.cpp
@@ -0,0 +1,22 @@
+/* We can generate subsets. and then find the difference between the highest element and lowest element. Then we will choose the subset with lowest difference.
+It will be better if we sort the array and then we choose consecutive elements and find the difference between highest and lowest element*/
+
+// n->packets , m->students
+int findMinDif(int arr[], int n, int m){
+  
+  if(m==0||n==0){ 
+    return -1;
+    
+  if(n<m){
+    return -1;
+    
+  int min_diff = INT_MAX;
+    
+  for(int i=0; i+m-1<n; i++){
+    int diff = arr[i+m-1] - arr[i];
+    if(diff < min_diff){
+      min_diff = diff;
+    }
+  }
+  return min_diff;    
+}    
