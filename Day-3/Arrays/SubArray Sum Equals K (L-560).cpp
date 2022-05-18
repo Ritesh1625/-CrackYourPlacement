38595ab24f84bcf8bc019adb02eb4884a40162f8
Ritesh Mahato <87099919+Ritesh1625@users.noreply.github.com>
2022-05-18 17:45:19 +0530

    Create SubArray Sum Equals K (L-560).cpp

---
+++ Day-3/Arrays/SubArray Sum Equals K (L-560).cpp
@@ -0,0 +1,29 @@
+// Brute force- taking two loops and calculating sum of all sub arrays.
+// Gere we will be using hash mhap.
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+    unordered_map<int, int>m;
+        int count =0;
+        int sum =0;
+        
+        int n=nums.size();
+        for(int i=0; i<n; i++){
+            sum+=nums[i];
+            
+            if(sum == k) count++;
+            
+            if(m.find(sum-k)!=m.end()){
+                count=m[sum-k]+count;
+            }
+            
+            if(m.find(sum)!=m.end()){
+                m[sum]++;
+            }else{
+                m[sum]=1;
+            }
+        }
+        return count;
+    }
+};
