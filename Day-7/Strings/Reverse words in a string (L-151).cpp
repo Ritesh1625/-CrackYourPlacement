958553b3d6ed330c53c413a814fc1cca8b2f2740
Ritesh Mahato <87099919+Ritesh1625@users.noreply.github.com>
2022-05-29 11:03:45 +0530

    Create Reverse words in a string (L-151).cpp

---
+++ Day-7/Strings/Reverse words in a string (L-151).cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n = s.size();
+        
+        stack <string> st;
+        string result;
+        for(int i=0; i<n; i++){
+            string word;
+            if(s[i] == ' ')
+                continue;
+            
+            while(i<n && s[i]!=' '){
+                word += s[i];
+                i++;
+            }
+            st.push(word);
+        }
+        while(!st.empty()){
+            result += st.top();
+            st.pop();
+            if(!st.empty()) 
+                result += " ";
+        }
+        return result;
+    }
+};
